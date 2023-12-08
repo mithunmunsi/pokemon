c3bc881230b6a5bdf3469c6f8644129c729c65a6
Mithun Munsi <mithun.munsi@gmail.com>
2023-12-08 05:01:13 +0200

    updated all files

--- main.js
+++ main.js
@@ -1,66 +1,115 @@
-const content = document.querySelector(".pokedex");
-const searchBar = document.querySelector(".searchBar");
-let pokeData = [];
-let hpCharacters = [];
+const pokeContent = document.querySelector('.pokeCards');
+const getGen = document.querySelectorAll('.btn');
+const numOfPokemon = document.querySelector('#pokemons');
+const search = document.querySelector('#search-box');
+const toTopBtn = document.querySelector('#backToTop');
 
-const fetchData = async () => {
-  await fetch("https://pokeapi.co/api/v2/pokemon?limit=121&offset=0")
-    .then((response) => response.json())
+let pokeData = [];
+
+// 1)) FETCHING DATA FROM POKEAPI
+
+const getPokemon = async (gen) => {
+  await fetch(`https://pokeapi.co/api/v2/generation/${gen}/`)
+    .then((res) => res.json())
     .then((data) => {
-      const fetches = data.results.map((item) => {
-        return fetch(item.url)
-          .then((res) => res.json())
+      const fetches = data.pokemon_species.map((species) => {
+        numOfPokemon.textContent = `There are ${data.pokemon_species.length} pokemons in the generation ${gen}`;
+
+        return fetch(`https://pokeapi.co/api/v2/pokemon/${species.name}/`)
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            } else if (res.status === 404) {
+              return Promise.reject('Page not found');
+            } else {
+              return Promise.reject('Something Went Wront!');
+            }
+          })
           .then((data) => {
             return {
               id: data.id,
               name: data.name,
-              img: data.sprites.other["official-artwork"].front_default,
+              img: data.sprites.other['official-artwork'].front_default,
               types: data.types,
+              weight: data.weight,
+              height: data.height,
             };
+          })
+          .catch((error) => {
+            return {};
           });
       });
       Promise.all(fetches).then((res) => {
         pokeData = res;
-        pokeCards();
+        pokeCards(pokeData);
       });
     });
 };
-
-// TODO:
-// 1. pokemon.types is an array, you need to map it again
-// 2. connect input and search from pokeDex array by using the .filter() method (https://www.jamesqquick.com/blog/build-a-javascript-search-bar/)
-
-const pokeCards = () => {
-  const cards = pokeData
-    .map((pokemon) => {
-      return `<div>
-  <img src="${pokemon.img}" alt="${pokemon.name}"/>
-  <p>${pokemon.id}</p>
-  <h2>${pokemon.name}</h2>
-  <div>
-  ${pokemon.types.map((type) => getTypeString(type)).join("")}
- 
-  </div>
-</div>`;
-    })
-    .join("");
-
-  content.innerHTML = cards;
-};
-
-const getTypeString = (type) => {
-  return `<p>${type.type.name}</p>`;
-};
-// Search and Filtering
-searchBar.addEventListener("keyup", (e) => {
-  const searchString = e.target.value.toLowerCase();
-  const filteredCharacters = hpCharacters.filter((character) => {
-    return (
-      character.name.includes(searchString) ||
-      character.house.includes(searchString)
-    );
+// 2)) ADDING DATA TO THE HTML PAGE
+function pokeCards(data) {
+  const cards = data.map((pokemon) => {
+    if (pokemon.types != undefined) {
+      return `<div class="card">
+            <div class="img"><img src=${pokemon.img}></div>
+            <div class="pokeName">${pokemon.name}</div>
+            <div class="pokeTypes">
+            ${pokemon.types.map((type) => getType(type)).join('')}
+            </div>
+            <div class=pokeDimensions>
+            <p>weight: ${pokemon.weight}</p>
+            <p>height: ${pokemon.height}</p>
+            </div>
+            <div class="pokeRank">#${pokemon.id}</div>
+            </div>`;
+    }
   });
-  displayCharacters(filteredCharacters);
+  pokeContent.innerHTML = cards;
+  search.classList.add('show');
+}
+
+function getType(type) {
+  return `<p>${type.type.name}</p>`;
+}
+
+getGen.forEach((el, i) => {
+  el.addEventListener('click', () => getPokemon(i + 1));
 });
-console.log(searchString);
-fetchData();
+
+// 3)) SEARCH HANDLER
+function searchHandler(input, array) {
+  let newPokedList = []; //an empty array of pokemons contains search inputs
+
+  if (input.length <= 0) {
+    pokeCards(pokeData);
+  } else {
+    const pokemons = array.filter(
+      (pokemon) => pokemon != undefined && pokemon.name != undefined
+    );
+    newPokedList = pokemons.filter((pokemon) => pokemon.name.includes(input));
+    pokeCards(newPokedList);
+  }
+}
+search.addEventListener('input', () => searchHandler(search.value, pokeData));
+
+// 4)) BACK TO TOP FUNCTION HANDLER
+// Browser onscroll event triger
+window.onscroll = function () {
+  scrollFunction();
+};
+// Code from W3S about scrolling to top
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    toTopBtn.style.display = 'block';
+  } else {
+    toTopBtn.style.display = '';
+  }
+}
+
+function getTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+toTopBtn.addEventListener('click', getTop);
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # Pokemon API
+
+live link here: https://public.bc.fi/s2301253/pokemon/
